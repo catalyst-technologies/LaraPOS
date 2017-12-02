f8b62057f2038bda7aed4ca5e6fe5112b86e06c4
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-02 14:56:14 +0800

    controllers create
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

---
+++ app/Http/Controllers/Products.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Products extends Controller {
+    public function all(){
+        
+    }
+    
+    public function single($id){
+        
+    }
+    
+    public function view($id){
+        
+    }
+    
+    public function create(Request $request){
+        
+    }
+    
+    public function update(Request $request, $id){
+        
+    }
+    
+    public function delete(Request $request, $id){
+        
+    }
+}
---
+++ app/Http/Controllers/Users.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Users extends Controller {
+    public function __construct() {
+        
+    }
+    
+    public function all(){
+        
+    }
+    
+    public function single($id){
+        
+    }
+    
+    public function create(Request $requesst){
+        
+    }
+    
+    public function update(Request $request, $id){
+        
+    }
+    
+    public function delete(Request $request, $id){
+        
+    }
+}
