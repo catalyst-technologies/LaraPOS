dfa0ed05931cc67d6b41dffb2255d53a0b5f1114
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-05 21:08:08 +0800

    apply users model as authentication model
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 
-class Users extends Model{
+class Users extends Model implements Authenticatable{
+    use AuthenticableTrait;
     protected $table = 'users';
 }
