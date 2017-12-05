9156b2eb25d95f6771915eaf353a4673fd5d89b2
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-05 20:55:31 +0800

    modify username to unique
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration {
     public function up() {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email', 50)->unique();
             $table->string('password');
             $table->smallInteger('user_type')->default(0);
