b9bd2c0eaa56470a67c5d71b32405ae660219e6c
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-07 08:00:01 +0800

    redirect change to root
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -26,7 +26,7 @@ use RegistersUsers;
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
