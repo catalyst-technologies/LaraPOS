9cbc1c700ccfd82379b7a9e4df30bff82241e72d
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-07 21:16:45 +0800

    change route link
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- resources/views/pages/users/create.blade.php
+++ resources/views/pages/users/create.blade.php
@@ -12,7 +12,7 @@
 <!-- Breadcrumb -->
 <ol class="breadcrumb">
     <li class="breadcrumb-item">
-        <a href="{{ route('root') }}">Dashboard</a>
+        <a href="{{ route('dashboard') }}">Dashboard</a>
     </li>
     <li class='breadcrumb-item'>
         <a href="{{ route('users.all') }}">Users</a>
