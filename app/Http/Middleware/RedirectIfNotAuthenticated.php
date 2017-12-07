7085e220fb40e28362a6631975d9bd327960ff27
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-07 21:07:06 +0800

    redirect to login if not authenticated
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- app/Http/Middleware/RedirectIfNotAuthenticated.php
+++ app/Http/Middleware/RedirectIfNotAuthenticated.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-
+use Illuminate\Support\Facades\Auth;
 class RedirectIfNotAuthenticated {
 
     /**
@@ -15,7 +15,7 @@ class RedirectIfNotAuthenticated {
      */
     public function handle($request, Closure $next, $guard = null) {
         if (!Auth::guard($guard)->check()) {
-            redirect('/login');
+            return redirect()->route('login');
         }
         return $next($request);
     }
