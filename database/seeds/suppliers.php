452624fbcdd30c368b1f15c1171c82d49b198a6d
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-09 03:16:25 +0800

    create supplier seeder
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

---
+++ database/seeds/suppliers.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Suppliers as SuppliersModel;
+class suppliers extends Seeder{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+        $this->create('National Book Store','Hannah Wong','hannah@wong.com','6395423485122','Tundo','Metro Manila','Manila','4444',1234);
+        $this->create('ENWA','Yun Lu','yunlu@enwa.org','63952235123','Centro','Dapitan City','Zamboanga Del Norte','7100',4135);
+    }
+    private function create($company,$name,$email,$phone_number,$address,$city,$state,$zip,$account){
+        $supplier = new SuppliersModel;
+        $supplier->company_name = $company;
+        $supplier->name = $name;
+        $supplier->email = $email;
+        $supplier->phone_number = $phone_number;
+        $supplier->address = $address;
+        $supplier->city = $city;
+        $supplier->state = $state;
+        $supplier->zip = $zip;
+        $supplier->account = $account;
+        $supplier->save();
+    }
+}
