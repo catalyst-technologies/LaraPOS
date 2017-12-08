c10894c26c510404461b0c0bbbc64253913ea97e
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-08 15:48:36 +0800

    modify products table
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- database/migrations/2017_10_24_033602_create_products_table.php
+++ database/migrations/2017_10_24_033602_create_products_table.php
@@ -1,28 +1,31 @@
- ]3t<?php
+]3t<?php
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
-{
+class CreateProductsTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('cat_id')->nullable();
+            $table->string('upc_ean_isbn', 90);
             $table->string('name');
+            $table->string('size', 20);
             $table->string('description');
-            $table->binary('img')->nullable();
+            $table->string('img')->default('no-foto.png');
             #$table->qty('int'); // we'll transfer this to inventory table
-            $table->integer('price');
+            #$table->integer('price');
+            $table->decimal('cost_price', 9, 2);
+            $table->decimal('selling_price', 9, 2);
             $table->integer('qty');
-            $table->date('expiry')->nullable();//remove nullable on final
+            $table->date('expiry')->nullable(); //remove nullable on final
+            $table->integer('type')->default(1);
             $table->timestamps();
         });
     }
@@ -32,8 +35,8 @@ class CreateProductsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('products');
     }
+
 }
