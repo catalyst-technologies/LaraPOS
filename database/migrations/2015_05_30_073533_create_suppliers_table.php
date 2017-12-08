98750d3f3c109a2bd5ffcdf82f5d22f88d37d611
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-09 03:04:58 +0800

    set comments to nullable
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- database/migrations/2015_05_30_073533_create_suppliers_table.php
+++ database/migrations/2015_05_30_073533_create_suppliers_table.php
@@ -22,7 +22,7 @@ class CreateSuppliersTable extends Migration {
             $table->string('city', 20);
             $table->string('state', 30);
             $table->string('zip', 10);
-            $table->text('comments');
+            $table->text('comments')->nullable();
             $table->string('account', 20);
             $table->timestamps();
         });
