7c9e3e6035f1309e6e3785d35c3771d01c268b33
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-09 03:16:37 +0800

    create users seeder
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

---
+++ database/seeds/users.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Users as UsersModel;
+
+class users extends Seeder{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+        $user = new UsersModel;
+        $user->username = "admin";
+        $user->email = "admin@larapos.com";
+        $user->password = bcrypt('12345');
+        $user->user_type = 0;
+        $user->save();
+
+        for($i = 0; $i < 4; $i++){
+            $user = new UsersModel;
+            $user->username = "staff$i";
+            $user->email = "staff$i@larapos.com";
+            $user->password = bcrypt('12345');
+            $user->user_type = 1;
+        }
+    }
+}
