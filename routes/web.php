ce6bb851239d79498a0cd17305a77f217ceb4aa0
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-09 03:23:07 +0800

    route fixes
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,76 @@
  */
 
 Auth::routes();
-Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/logout', 'Auth\LoginController@logout')
+	->name('logout');
 
-Route::get('/', 'Dashboard@show')->name('dashboard')->middleware('redirectIfNoAuth');
+Route::get('/', 'Dashboard@show')
+	->name('dashboard')
+	->middleware('redirectIfNoAuth');
 
-Route::get('/users', 'Users@all')->name('users.all')->middleware('redirectIfNoAuth');
-Route::get('/users/create', 'Users@create')->name('users.create')->middleware('redirectIfNoAuth');
-Route::post('/users/save', 'Users@save')->name('users.save')->middleware('redirectIfNoAuth');
-Route::get('/users/edit/{id}', 'Users@edit')->name('users.edit')->middleware('redirectIfNoAuth');
-Route::get('/users/view/{id}', 'Users@view')->name('users.view')->middleware('redirectIfNoAuth');
-Route::post('/users/update/{id}', 'Users@update')->name('users.update')->middleware('redirectIfNoAuth');
-Route::get('/users/delete/{id}', 'Users@delete')->name('users.delete')->middleware('redirectIfNoAuth');
+# User Routes
+Route::get('/users', 'Users@all')
+	->name('users.all')
+	->middleware('redirectIfNoAuth','authAdminOnly');
+Route::get('/users/create', 'Users@create')
+	->name('users.create')
+	->middleware('redirectIfNoAuth','authAdminOnly');
+Route::post('/users/save', 'Users@save')
+	->name('users.save')
+	->middleware('redirectIfNoAuth','authAdminOnly');
+Route::get('/users/edit/{id}', 'Users@edit')
+	->name('users.edit')
+	->middleware('redirectIfNoAuth','authAdminOnly');
+Route::get('/users/view/{id}', 'Users@view')
+	->name('users.view')
+	->middleware('redirectIfNoAuth','authAdminOnly');
+Route::post('/users/update/{id}', 'Users@update')
+	->name('users.update')
+	->middleware('redirectIfNoAuth','authAdminOnly');
+Route::get('/users/delete/{id}', 'Users@delete')
+	->name('users.delete')
+	->middleware('redirectIfNoAuth','authAdminOnly');
 
-Route::get('/products', 'Products@all')->name('products.all')->middleware('redirectIfNoAuth');
-Route::get('/products/view/{id}', 'Products@view')->name('products.view')->middleware('redirectIfNoAuth');
-Route::get('/products/{id}/edit', ' Products@edit')->name('products.edit')->middleware('redirectIfNoAuth');
-Route::put('/products/{id}/update', 'Products@update')->name('products.update')->middleware('redirectIfNoAuth');
-Route::get('/products/{id}/delete', 'Products@delete')->name('products.delete')->middleware('redirectIfNoAuth');
-Route::get('/products/create', 'Products@create')->name('products.create')->middleware('redirectIfNoAuth');
-Route::post('/products/save', 'Products@save')->name('products.save')->middleware('redirectIfNoAuth');
 
-Route::get('/orders', 'Orders@all')->name('orders.all')->middleware('redirectIfNoAuth');
-Route::get('/orders/{id}', 'Orders@single')->name('orders.single')->middleware('redirectIfNoAuth');
-Route::post('/orders/{id}/update', 'Orders@update')->name('orders.update')->middleware('redirectIfNoAuth');
-Route::post('/orders/{id}/delete', 'Orders@delete')->name('orders.delete')->middleware('redirectIfNoAuth');
-Route::get('/orders/by/{user_id}', 'Orders@byUser')->name('orders.users')->middleware('redirectIfNoAuth');
-Route::post('/orders/create', 'Orders@create')->name('orders.create')->middleware('redirectIfNoAuth');
+# Customer Routes
+Route::get('/customers','Customers@all')
+	->name('customers.all');
+Route::get('/customers/create','Customers@create')
+	->name('customers.create');
+Route::get('/customers/edit/{id}','Customers@edit')
+	->name('customers.edit');
+Route::get('/customers/delete/{id}','Customers@delete')
+	->name('customers.delete');
+Route::post('/customers/save','Customers@save')
+	->name('customers.save');
+Route::post('/customers/update/{id}','Customers@update')
+	->name('customers.update');
+
+# Supplier routes
+Route::get('/suppliers','Suppliers@all')
+	->name('suppliers.all');
+Route::get('/suppliers/create','Suppliers@create')
+	->name('suppliers.create');
+Route::get('/suppliers/edit/{id}','Suppliers@edit')
+	->name('suppliers.edit');
+Route::get('/suppliers/delete/{id}','Suppliers@delete')
+	->name('suppliers.delete');
+Route::post('/suppliers/save','Suppliers@save')
+	->name('suppliers.save');
+Route::post('/suppliers/update/{id}','Suppliers@update')
+	->name('suppliers.update');
+
+
+# Item routes
+Route::get('/items','Items@all')
+	->name('items.all');
+Route::get('/items/create','Items@create')
+	->name('items.create');
+Route::get('/items/edit/{id}','Items@edit')
+	->name('items.edit');
+Route::get('/items/delete/{id}','Items@delete')
+	->name('items.delete');
+Route::post('/items/save','Items@save')
+	->name('items.save');
+Route::post('/items/update/{id}','Items@update')
+	->name('items.update');
