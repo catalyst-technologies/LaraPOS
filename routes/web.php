91c670da4e876beae1f20cc406d64cd54457021f
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-11 18:59:56 +0800

    middleware applied and sales routes added
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -45,48 +45,78 @@ Route::get('/users/delete/{id}', 'Users@delete')
 
 # Customer Routes
 Route::get('/customers','Customers@all')
-	->name('customers.all');
+	->name('customers.all')
+        ->middleware('redirectIfNoAuth');
 Route::get('/customers/create','Customers@create')
-	->name('customers.create');
+	->name('customers.create')
+        ->middleware('redirectIfNoAuth');
 Route::get('/customers/edit/{id}','Customers@edit')
-	->name('customers.edit');
+	->name('customers.edit')
+        ->middleware('redirectIfNoAuth');
 Route::get('/customers/delete/{id}','Customers@delete')
-	->name('customers.delete');
+	->name('customers.delete')
+        ->middleware('redirectIfNoAuth');
 Route::post('/customers/save','Customers@save')
-	->name('customers.save');
+	->name('customers.save')
+        ->middleware('redirectIfNoAuth');
 Route::post('/customers/update/{id}','Customers@update')
-	->name('customers.update');
+	->name('customers.update')
+        ->middleware('redirectIfNoAuth');
 
 # Supplier routes
 Route::get('/suppliers','Suppliers@all')
-	->name('suppliers.all');
+	->name('suppliers.all')
+        ->middleware('redirectIfNoAuth');
 Route::get('/suppliers/create','Suppliers@create')
-	->name('suppliers.create');
+	->name('suppliers.create')
+        ->middleware('redirectIfNoAuth');
 Route::get('/suppliers/edit/{id}','Suppliers@edit')
-	->name('suppliers.edit');
+	->name('suppliers.edit')
+        ->middleware('redirectIfNoAuth');
 Route::get('/suppliers/delete/{id}','Suppliers@delete')
-	->name('suppliers.delete');
+	->name('suppliers.delete')
+        ->middleware('redirectIfNoAuth');
 Route::post('/suppliers/save','Suppliers@save')
-	->name('suppliers.save');
+	->name('suppliers.save')
+        ->middleware('redirectIfNoAuth');
 Route::post('/suppliers/update/{id}','Suppliers@update')
-	->name('suppliers.update');
+	->name('suppliers.update')
+        ->middleware('redirectIfNoAuth');
 
 
 # Item routes
 Route::get('/items','Items@all')
-	->name('items.all');
+	->name('items.all')
+        ->middleware('redirectIfNoAuth');
 Route::get('/items/create','Items@create')
-	->name('items.create');
+	->name('items.create')
+        ->middleware('redirectIfNoAuth');
 Route::get('/items/edit/{id}','Items@edit')
-	->name('items.edit');
+	->name('items.edit')
+        ->middleware('redirectIfNoAuth');
 Route::get('/items/delete/{id}','Items@delete')
-	->name('items.delete');
+	->name('items.delete')
+        ->middleware('redirectIfNoAuth');
 Route::post('/items/save','Items@save')
-	->name('items.save');
+	->name('items.save')
+        ->middleware('redirectIfNoAuth');
 Route::post('/items/update/{id}','Items@update')
-	->name('items.update');
+	->name('items.update')
+        ->middleware('redirectIfNoAuth');
 
 Route::get('/inventory/{id}/edit','Inventory@show')
-	->name('inventory.edit');
+	->name('inventory.edit')
+        ->middleware('redirectIfNoAuth');
 Route::post('/inventory/{id}/update','Inventory@update')
-	->name('inventory.update');
+	->name('inventory.update')
+        ->middleware('redirectIfNoAuth');
+
+
+Route::get('/sales','Sales@show')
+	->name('sales')
+        ->middleware('redirectIfNoAuth');
+
+
+
+Route::resource('api/item', 'API\Items');
+Route::resource('api/saletemp', 'API\SaleTemp');
