5c63101d9a2f41fb9772d68cfe104d76938129f1
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-12 22:33:26 +0800

    show customers and sale data, and finished [complete sale] function
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- app/Http/Controllers/Sales.php
+++ app/Http/Controllers/Sales.php
@@ -2,10 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\Models\Sales as SalesModel;
+use App\Models\Customers as CustomersModel;
+use App\Models\SaleTemp as SaleTempModel;
+use App\Models\SalesItem as SalesItemModel;
+use App\Models\Items as ItemsModel;
+use App\Models\Inventories as InventoryModel;
 use Illuminate\Http\Request;
 
-class Sales extends Controller{
-    public function show(){
-        return view('pages.sales.main');
+class Sales extends Controller {
+
+    public function show() {
+        $sale = SalesModel::orderBy('id', 'desc')->first();
+        $customers = CustomersModel::select('name', 'id')->get();
+        return view('pages.sales.main')
+                        ->with('sale', $sale)
+                        ->with('customers', $customers);
     }
+
+    public function save(Request $request) {
+        $sales = new SalesModel;
+        $sales->customer_id = $request->input('customer_id');
+        $sales->user_id = Auth::user()->id;
+        $sales->payment_type = $request->input('payment_type');
+        $sales->comments = $request->input('comments');
+        $sales->save();
+
+        $saleItemTemp = SaleTempModel::all();
+        foreach ($saleItemTemp as $value) {
+            $saleItemData = new SalesItemModel;
+            $saleItemData->sale_id = $sales->id;
+            $saleItemData->item_id = $value->item_id;
+            $saleItemData->cost_price = $value->cost_price;
+            $saleItemData->selling_price = $value->selling_price;
+            $saleItemData->quantity = $value->quantity;
+            $saleItemData->total_cost = $value->total_cost;
+            $saleItemData->total_selling = $value->total_selling;
+            $saleItemData->save();
+
+            #Process inventory
+            $items = ItemsModel::find($value->item_id);
+            $inventory = new InventoryModel;
+            $inventory->item_id = $value->item_id;
+            $inventory->user_id = Auth::user()->id;
+            $inventory->in_out_qty = - ($value->quantity);
+            $inventory->remarks = "Sale " . $sales->id;
+            $inventory->save();
+            # process item quantity
+            $items->quantity = $items->quantity - $value->quantity;
+            $items->save();
+        }
+
+        SaleTempModel::truncate();
+        $itemssale = SalesItemModel::where('sale_id',$saleItemData->sale_id)->get();
+        flash()->success('Sales Completed');
+        return redirect()->route('sales');
+    }
+
 }
