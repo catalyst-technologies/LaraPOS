e3d0556da7008c42ad5e00339475195868816f9a
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-28 12:12:53 +0800

    add branch factor in seeder
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- database/seeds/users.php
+++ database/seeds/users.php
@@ -2,28 +2,43 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Users as UsersModel;
+use App\Models\Branches as BranchModel;
 
-class users extends Seeder{
+class users extends Seeder {
+
+    private $branches = [];
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run(){
-        $user = new UsersModel;
-        $user->username = "admin";
-        $user->email = "admin@larapos.com";
-        $user->password = bcrypt('12345');
-        $user->user_type = 0;
-        $user->save();
+    public function run() {
+        $this->get_branches();
 
-        for($i = 0; $i < 4; $i++){
-            $user = new UsersModel;
-            $user->username = "staff$i";
-            $user->email = "staff$i@larapos.com";
-            $user->password = bcrypt('12345');
-            $user->user_type = 1;
-            $user->save();
+        $this->create_user('admin', 0);
+        $this->create_user('staff1', 1);
+        $this->create_user('staff2', 1);
+        $this->create_user('staff3', 1);
+        $this->create_user('staff4', 1);
+        $this->create_user('staff5', 1);
+    }
+
+    private function get_branches() {
+        $branches = BranchModel::select('id')->get();
+        foreach ($branches as $branch) {
+            $this->branches[] = $branch->id;
         }
     }
+
+    private function create_user($name, $uType) {
+        $user = new UsersModel;
+        $user->username = $name;
+        $user->email = $name . '@larapos.com';
+        $user->password = bcrypt('12345');
+        $user->user_type = $uType;
+        $user->branch = array_random($this->branches);
+        $user->save();
+    }
+
 }
