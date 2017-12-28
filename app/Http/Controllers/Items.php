28286dbff73ba5edc212da60c3a01d0e0fa251b3
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-28 13:20:20 +0800

    prettify
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- app/Http/Controllers/Items.php
+++ app/Http/Controllers/Items.php
@@ -1,26 +1,29 @@
 <?php
 
 /**
- @todo: Transform this controller into a resource route
+  @todo: Transform this controller into a resource route
  */
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\Items as ItemsModel;
 use App\Models\Inventories as InventoryModel;
 
-class Items extends Controller{
-    public function all(){
+class Items extends Controller {
+
+    public function all() {
         $items = ItemsModel::get();
-    	return view('pages.items.all')->with([
-            'items' => $items,
+        return view('pages.items.all')->with([
+                    'items' => $items,
         ]);
     }
-    public function create(){
-    	return view('pages.items.create');
+
+    public function create() {
+        return view('pages.items.create');
     }
-    public function save(Request $request){
+
+    public function save(Request $request) {
         # save to items
         $item = new ItemsModel;
         $item->upc_ean_isbn = $request->input('upc_ean_isbn');
@@ -29,54 +32,57 @@ class Items extends Controller{
         $item->description = $request->input('description');
         $item->cost_price = $request->input('cost_price');
         $item->selling_price = $request->input('selling_price');
-        $item->quantity = $request->input('quantity');
-        if($item->save()){
+        if ($item->save()) {
             flash()->success('Item created successfully');
             # Save to inventory
             $inventory = new InventoryModel;
             $inventory->item_id = $item->id;
-			$inventory->user_id = 1; # todo: put current logged in user
-			$inventory->in_out_qty = $request->input('quantity');
-			$inventory->remarks = 'Manual Edit of Quantity';
-            if($inventory->save()){
+            $inventory->user_id = 1; # todo: put current logged in user
+            $inventory->in_out_qty = $request->input('quantity');
+            $inventory->remarks = 'Manual Edit of Quantity';
+            if ($inventory->save()) {
                 flash()->success('Inventory report created successfully');
-            }else{
+            } else {
                 flash()->error('Failed to create inventory report');
             }
             return redirect()->route('items.all');
-        }else{
+        } else {
             flash()->error('Failed to create item');
             return redirect()->route('items.create');
         }
     }
-    public function edit($id){
-        $item = ItemsModel::where('id',$id)->first();
-    	return view('pages.items.edit')->with([
-            'item' => $item,
+
+    public function edit($id) {
+        $item = ItemsModel::where('id', $id)->first();
+        return view('pages.items.edit')->with([
+                    'item' => $item,
         ]);
     }
-    public function update(Request $request,$id){
-        $item = ItemsModel::where('id',$id)->first();
+
+    public function update(Request $request, $id) {
+        $item = ItemsModel::where('id', $id)->first();
         $item->upc_ean_isbn = $request->input('upc_ean_isbn');
         $item->item_name = $request->input('item_name');
         $item->size = $request->input('size');
         $item->description = $request->input('description');
         $item->cost_price = $request->input('cost_price');
         $item->selling_price = $request->input('selling_price');
-        if($item->save()){
+        if ($item->save()) {
             flash()->success('Item updated successfully');
-        }else{
+        } else {
             flash()->error('Failed to update item');
         }
-        return redirect()->route('items.edit',['id'=>$id]);
+        return redirect()->route('items.edit', ['id' => $id]);
     }
-    public function delete(Request $request){
-        $item = ItemsModel::where('id',$id);
-        if($item->delete()){
+
+    public function delete(Request $request) {
+        $item = ItemsModel::where('id', $id);
+        if ($item->delete()) {
             flash()->success('Item deleted successfully');
-        }else{
+        } else {
             flash()->error('Failed to delete item');
         }
         return redirect('items.all');
     }
+
 }
