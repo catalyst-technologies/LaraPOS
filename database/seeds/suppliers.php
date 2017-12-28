5b7ab2da82409f1099c219a258225e2321d8029e
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-28 10:33:52 +0800

    add one more supplier in seeder
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- database/seeds/suppliers.php
+++ database/seeds/suppliers.php
@@ -11,6 +11,7 @@ class suppliers extends Seeder{
     public function run(){
         $this->create('National Book Store','Hannah Wong','hannah@wong.com','6395423485122','Tundo','Metro Manila','Manila','4444',1234);
         $this->create('ENWA','Yun Lu','yunlu@enwa.org','63952235123','Centro','Dapitan City','Zamboanga Del Norte','7100',4135);
+        $this->create('Books for Green','Earth Chun','earth_chun@universe.com','131231312313','Central Barangay','Dipolog City','Zamboanga Del Norte','7100',1413);
     }
     private function create($company,$name,$email,$phone_number,$address,$city,$state,$zip,$account){
         $supplier = new SuppliersModel;
