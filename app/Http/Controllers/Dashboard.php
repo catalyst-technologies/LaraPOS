c7a6267e740e442f90fe7bde3f76aaf175b5f72c
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-28 10:34:37 +0800

    add base code for the dashboard sales graph
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -3,9 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class Dashboard extends Controller {
-    public function show(){
+
+    public function show() {
+
         return view('pages.dashboard.main')->with([]);
     }
+
+    public function get_sales($by = 'day') {
+        switch ($by) {
+            case 'day':
+                $this->get_sales_by_day();
+                break;
+            case 'month':
+
+                break;
+            case 'year':
+
+                break;
+        }
+    }
+
+    private function get_sales_by_day() {
+        $date = Carbon::now();
+        $year = $date->year;
+        $month = $date->month;
+        
+        echo 'Year: ' . $year;
+        echo 'Month: ' . $month;
+        
+        
+    }
+
+    private function get_sales_by_month() {
+        $date = Carbon::now();
+        
+    }
+
+    private function get_sales_by_year() {
+        $date = Carbon::now();
+        
+    }
+
 }
