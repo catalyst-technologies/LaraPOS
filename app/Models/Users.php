573524251fa9ca74909a64765616000ccf5bcc71
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-28 13:19:29 +0800

    dirty work-around added
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use App\Models\Branches;
 
 class Users extends Model implements Authenticatable {
 
@@ -13,7 +14,9 @@ class Users extends Model implements Authenticatable {
     protected $table = 'users';
 
     public function branch() {
-        return $this->belongsTo('App\Models\Branches');
+        // return $this->belongsTo('App\Models\Branches');
+        # Dirty Solution (subject to change)
+        return Branches::where('id',$this->branch_id)->first();
     }
 
 }
