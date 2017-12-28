4cac932a7fe11be521f2e26923e16993cf60ee92
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-28 09:51:27 +0800

    add relations and auto resolve function
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- app/Models/Receiving.php
+++ app/Models/Receiving.php
@@ -4,7 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Receiving extends Model
-{
-    //
+use App\Models\ReceivingItem as ReceivingItemsModel;
+
+class Receiving extends Model {
+
+    public function user() {
+        return $this->belongsTo('App\Models\Users');
+    }
+
+    public function supplier() {
+        return $this->belongsTo('App\Models\Suppliers');
+    }
+    
+    public function items_received(){
+        $count = ReceivingItemsModel::where('receiving_id',$this->id)->sum('quantity');
+        return $count;
+    }
+    
+    public function total_cost(){
+        $cost = ReceivingItemsModel::where('receiving_id',$this->id)->sum('total_cost');
+        return $cost;
+    }
+    
 }
