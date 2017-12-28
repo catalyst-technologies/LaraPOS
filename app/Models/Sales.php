f94ff5f408210c07720754cb853df0c80bb005b5
Mark Clu Heramis <chumheramis@gmail.com>
2017-12-28 09:51:46 +0800

    add relations and auto-resolve functions
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- app/Models/Sales.php
+++ app/Models/Sales.php
@@ -2,8 +2,33 @@
 
 namespace App\Models;
 
+use App\Models\SalesItem as SalesItemModel;
 use Illuminate\Database\Eloquent\Model;
 
 class Sales extends Model {
+
     protected $table = 'sales';
+
+    public function items_purchased() {
+        $count = SalesItemModel::where('sale_id', $this->id)->sum('quantity');
+        return $count;
+    }
+
+    public function total_cost() {
+        $cost = SalesItemModel::where('sale_id', $this->id)->sum('total_cost');
+        return $cost;
+    }
+    public function profit(){
+        $sell_price = SalesItemModel::where('sale_id',$this->id)->sum('total_selling');
+        $cost_price = SalesItemModel::where('sale_id', $this->id)->sum('total_cost');
+        return $sell_price - $cost_price;
+    }
+    public function user() {
+        return $this->belongsTo('App\Models\Users');
+    }
+
+    public function customer() {
+        return $this->belongsTo('App\Models\Customers');
+    }
+
 }
