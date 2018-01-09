5f9d98d862b1ebe1e7276b15ba6dc3069688fd1c
Mark Clu Heramis <chumheramis@gmail.com>
2018-01-09 16:41:11 +0800

    prettify
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- app/Http/Controllers/Items.php
+++ app/Http/Controllers/Items.php
@@ -19,30 +19,30 @@ class Items extends Controller {
 
     public function __construct() {
         $this->data['_branch'] = \App\Models\Branches::get();
-        if(empty(Session::get('branch'))) Session::put('branch',0);
+        if (empty(Session::get('branch')))
+            Session::put('branch', 0);
     }
 
     public function all() {
         $items = null;
 
-        if(Session::get('branch')==0){
-          $this->data['items'] = ItemsModel::get();
-          }else{
-          $this->data['items'] = ItemsModel::where('branch_id',Session::get('branch'))
-          ->get();
-          }        
-        
-			  $tdata = $this->data['items'];  
-				$currentPage = LengthAwarePaginator::resolveCurrentPage();
-				$perPage = 10;		
-				
-				$itemCollection = collect($tdata);		
-				$currentPageItems = $itemCollection->slice(($currentPage * $perPage) - $perPage, $perPage)->all();
-				
-				$paginatedItems= new LengthAwarePaginator($currentPageItems , count($itemCollection), $perPage );
-				$paginatedItems->setPath(request()->url());
-        
-        return view('pages.items.all')->withItems($paginatedItems)->with('_branch', $this->data['_branch']);   
+        if (Session::get('branch') == 0) {
+            $this->data['items'] = ItemsModel::get();
+        } else {
+            $this->data['items'] = ItemsModel::where('branch_id', Session::get('branch'))
+                    ->get();
+        }
+
+        $tdata = $this->data['items'];
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 10;
+
+        $itemCollection = collect($tdata);
+        $currentPageItems = $itemCollection->slice(($currentPage * $perPage) - $perPage, $perPage)->all();
+
+        $paginatedItems = new LengthAwarePaginator($currentPageItems, count($itemCollection), $perPage);
+        $paginatedItems->setPath(request()->url());
+        return view('pages.items.all')->withItems($paginatedItems)->with('_branch', $this->data['_branch']);
     }
 
     public function create() {
