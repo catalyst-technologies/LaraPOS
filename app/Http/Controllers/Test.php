f5ff434ae95f88c64d440935bcac92ea4adbad04
Mec Mec <wconejar@gmail.com>
2018-01-17 02:16:06 +0800

    group by item names

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -7,11 +7,20 @@ use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Models\Users as UsersModel;
 use App\Models\Items as ItemsModel;
+use Illuminate\Support\Facades\DB;
 
 class Test extends Controller {
 
     public function index() {
-        for($i=1; $i<7;$i++){
+      $items = DB::table('items')
+                   ->select(DB::raw('SUM(quantity) as item_count, item_name'))
+                   ->groupBy('item_name')
+                   ->get();
+
+      $this->display_json($items);
+
+
+        /** for($i=1; $i<7;$i++){
 
           $start = Carbon::now()->startOfMonth()->subMonth($i);
           $end   = Carbon::now()->endOfMonth()->subMonth($i);
@@ -24,8 +33,10 @@ class Test extends Controller {
           echo $end->toDateTimeString();
 
           echo "</br>";
-        }
+        } **/
       }
+
+
 
     private function display_json($json){
         echo '<pre>' . json_encode($json,JSON_PRETTY_PRINT) . '</pre>';
