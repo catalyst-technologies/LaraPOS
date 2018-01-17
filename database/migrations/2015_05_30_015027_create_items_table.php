c71782798fcf6ff9b1d8fa01895911c42aa97679
Mark Clu Heramis <chumheramis@gmail.com>
2018-01-18 03:41:58 +0800

    changes in database
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- database/migrations/2015_05_30_015027_create_items_table.php
+++ database/migrations/2015_05_30_015027_create_items_table.php
@@ -13,14 +13,14 @@ class CreateItemsTable extends Migration {
     public function up() {
         Schema::create('items', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('upc_ean_isbn', 90);
-            $table->string('item_name', 90);
+            $table->string('upc_ean_isbn', 90)->unique();
+            $table->string('item_name', 90)->unique();
             $table->string('size', 20);
             $table->text('description')->nullable();
             $table->string('avatar', 255)->default('no-foto.png');
             $table->decimal('cost_price', 9, 2);
             $table->decimal('selling_price', 9, 2);
-            $table->integer('quantity');
+            #$table->integer('quantity');
             $table->date('expiry')->nullable();
             $table->integer('created_by')->unsigned();
             $table->integer('branch_id')->unsigned();
